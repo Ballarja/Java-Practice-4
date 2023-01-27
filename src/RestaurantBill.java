4517bbc8d7a3276a1387d1b4326712cd2a789995
Ballarja <JamesBallard87@hotmail.com>
2023-01-27 11:56:02 -0600

    added extra method to RestaurantBill class for practice.

--- src/RestaurantBill.java
+++ src/RestaurantBill.java
@@ -2,10 +2,15 @@ public class RestaurantBill {
 
     public static void main(String args[]){
         tipTheWaiter(35.12);
+        foodOrdered("12 oz Steak with mash potatoes");
     }
+
     public static void tipTheWaiter(double bill){
         double amountToTip = bill * 0.15;
 
         System.out.println("Your service was wonderful! Please, accept this tip: "+ amountToTip );
+    }
+    public static void foodOrdered(String entreeOrdered){
+        System.out.println(entreeOrdered);
     }
 }
