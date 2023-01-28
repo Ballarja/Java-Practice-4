807f5e3fe2e84295fcc68106c7262239ffbc255a
Ballarja <JamesBallard87@hotmail.com>
2023-01-27 23:02:40 -0600

    practiced using void return values

--- src/ReturnValues.java
+++ src/ReturnValues.java
@@ -2,16 +2,18 @@ public class ReturnValues {
 
     public static void main(String args[]){
         double area1 = calculateArea(2.3, 3.6);
-        double area2 = calculateArea(1.6, 2.4);
-        double area3 = calculateArea(2.6, 4.2);
+        printArea(2.3, 3.6, area1);
 
         String englishExplanation = explainArea("English");
-        String frenchExplanation = explainArea("French");
-        String spanishExplanation = explainArea("Spanish");
-        String italianExplanation = explainArea("Italian");
+
+
     }
 
     public static double calculateArea(double length, double width){
+        if(length < 0 || width < 0){
+            System.out.println("INVALID DIMENSIONS");
+            System.exit(0);
+        }
         double area = length * width;
         return area;
     }
@@ -23,6 +25,10 @@ public class ReturnValues {
            case "Spanish": return "area es igual a largo * ancho";
            default: return "Language not available";
        }
+    }
+
+    public static void printArea(double length, double width, double area){
+        System.out.println("A rectangle with a length of " + length + " and a width of " + width + " has an area of " + area);
     }
 }
 
