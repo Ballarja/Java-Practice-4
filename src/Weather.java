0cda32cbf01e73a6a0f016155ec1918b0d42f31c
Ballarja <JamesBallard87@hotmail.com>
2023-02-04 21:06:51 -0600

    Starting on Workbook 4.3

--- src/Weather.java
+++ src/Weather.java
@@ -1,3 +1,8 @@
 //workshop for part 4 of tutorial
 public class Weather {
+    public static void main(String[] args){
+        double noon = 77;
+        double evening = 61;
+        double midnight = 55;
+    }
 }
