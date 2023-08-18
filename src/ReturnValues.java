b6e5c01f6237eae4b46c31e43f7d5aa7a5525c73
Ballarja <JamesBallard87@hotmail.com>
2023-08-18 11:27:31 -0500

    added on to ReturnValues.java for practice with return values.

--- src/ReturnValues.java
+++ src/ReturnValues.java
@@ -1,11 +1,25 @@
 public class ReturnValues {
 
     public static void main (String args[]){
-        calculateArea(4.3, 7.5);
+        double area = calculateArea(4.3, 7.5);
+
+        String englishExplanation = explainArea("English");
+        String frenchExplanation = explainArea("French");
+        String spanishExplanation = explainArea("Spanish");
+        String italianExplanation = explainArea("Italian");
     }
 
     public static double calculateArea(double length, double width){
         double area = length * width;
         return area;
+    }
+
+    public static String explainArea(String language){
+        switch (language) {
+            case "English": return "Area equals length * width";
+            case "French": return "La surface est egale a la longueur * la largeur";
+            case "Spanish": return "area es igual a largo * ancho";
+            default: return "Language not available";
+        }
     }
 }
